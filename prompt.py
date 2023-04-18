89d10b01d7c09bdb14558b7014e3799b32452347
John-Charles D. Sokolow <jc@styleseat.com>
2023-04-18 11:49:19 -0500

    more work

--- prompt.py
+++ prompt.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
 from subprocess import Popen, PIPE, DEVNULL
+
+def without(input, char):
+    return [x for x in input if x != char]
 
 
 def call_git(*args):
@@ -24,6 +28,38 @@ def get_untracked():
 
     return len(found)
 
+
+STATES = {
+    "A": "added",
+    "M": "modified",
+    "T": "type_change",
+    "D": "deleted",
+    "R": "renamed",
+    "C": "copied",
+}
+
+
+def get_status():
+    index_status = defaultdict(int)
+    worktree_status = defaultdict(int)
+
+    result = call_git("status", "--porcelain").split("\n")
+    for file in without(result, ""):
+        index_state = file[0]
+        worktree_state = file[1]
+
+        if index_state != " ":
+            index_status[STATES[index_state]] += 1
+
+        if worktree_state != " ":
+            worktree_status[STATES[worktree_state]] += 1
+
+    return {
+        "index_status": index_status,
+        "worktree_status": worktree_status
+    }
+
+
 def get_branch():
     result = call_git("branch", "--show-current")
     branch_name = result.strip()
@@ -43,19 +79,53 @@ def get_in_git_repo():
     return result.strip() == "true"
 
 
+def build_status_line(status):
+    parts = []
+    added = status["added"] + status["copied"]
+    if added > 0:
+        parts.append(f"\x1b[97;0m{added}+")
+
+    modified = status["modified"] + status["renamed"] + status["type_change"]
+    if modified:
+        parts.append(f"\x1b[93;0m{modified}∆")
+
+    if status["deleted"] > 0:
+        parts.append(f"\x1b[0;41m{status['deleted']}-")
+
+    return " ".join(parts)
+
+
+def build_status_lines(status):
+    parts = []
+    index_line = build_status_line(status["index_status"])
+    worktree_line = build_status_line(status["worktree_status"])
+
+    if index_line != "":
+        parts.append(index_line)
+
+    if worktree_line != "":
+        parts.append(worktree_line)
+
+    return " \x1b[0m/ ".join(parts)
+
+
 if __name__ == '__main__':
     if get_in_git_repo():
-        current_branch = get_branch()
-        tracked_files = get_files("-c")
-        untracked_files = get_untracked()
         stashes = get_shashes()
+        current_branch = get_branch()
+        status_line = build_status_lines(get_status())
 
-        prompt = " ".join([x for x in [
-            f"[\x1b[0;35m{current_branch}",
-            "\x1b[0m|",
-            f"\x1b[0;34m{tracked_files}+" if tracked_files > 0 else "",
-            f"\x1b[0;31m{untracked_files}\u26A0" if untracked_files > 0 else "",
-            f"\x1b[0;37m{stashes}\u2691" if stashes > 0 else ""
-        ] if x != ""]) + "\x1b[0m]"
+        prompt_parts = [
+            f"[\x1b[0;35m{current_branch} \x1b[0m|"
+        ]
+
+        if status_line != "":
+            prompt_parts.append(status_line)
+
+        if stashes > 0:
+            prompt_parts.append(f"\x1b[0;37m{stashes}\u2691")
+
+        prompt = " ".join(prompt_parts) + "\x1b[0m]"
+
 
         print(prompt)
